f7789080ca490e85198d5b49749c90fd6ee85380
Alan Colon <alan.colon@cybercoders.com>
2019-01-17 14:03:51 -0800

    Add basic websocket functionality

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
+const client = require('./client')
+
 document.addEventListener('DOMContentLoaded', () => {
+  client.connect()
+  client.addEventListener('open', () => {
+    client.send('Hello Server')
+  })
+  client.addEventListener('message', evt => {
+    console.log(evt.data)
+  })
 })
