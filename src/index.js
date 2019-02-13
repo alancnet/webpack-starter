10f2ad0266858c9db052a6330216cb26e197ffaa
Alan Colon <github@alanc.net>
2019-02-13 13:43:50 -0800

    ...

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-const client = require('./client')
+require('material-icons/iconfont/material-icons.css')
+require('material-design-lite/dist/material.indigo-green.min.css')
+
+
+const angular = require('angular')
+angular.module('app', [])
+
+const serviceWorkerFile = require('file-loader!./service-worker.js')
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations()
+    .then(regs => Promise.all(regs.map(reg => reg.unregister())))
+    .then(() => navigator.serviceWorker.register(serviceWorkerFile))
+  
+  .then(function(registration) {
+    console.log('Registration successful, scope is:', registration.scope);
+  })
+  .catch(function(error) {
+    console.log('Service worker registration failed, error:', error);
+  });
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-  client.connect()
-  client.addEventListener('open', () => {
-    client.send('Hello Server')
-  })
-  client.addEventListener('message', evt => {
-    console.log(evt.data)
-  })
 })
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,17 @@ const LiveReloadPlugin = require('webpack-livereload-plugin')
 module.exports = {
   entry: './src/index.js',
   mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin(),
     new LiveReloadPlugin({
